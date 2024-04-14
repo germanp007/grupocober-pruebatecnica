fb8c22342c425ac1f9206b045951f35307c6b9b2
germanp007 <pintogerman281@gmail.com>
2024-04-14 17:53:04 -0300

    agregando nuevas caracteristicas

--- backend/server.js
+++ backend/server.js
@@ -21,9 +21,43 @@ server.post("/api/enviar-correo", async (req, res) => {
     const { email } = req.body;
     const result = await transporter.sendMail({
       from: "pintogerman281@gmail.com",
-      to: "pintogerman281@gmail.com",
+      to: "admin@grupocober.online",
       subject: "nuevo suscriptor",
-      text: `Felicidades, tienes un nuevo suscriptor ${email}`,
+      html: `
+      <head>
+      <style>
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #f4f4f4;
+        margin: 0;
+        padding: 0;
+      }
+      .container {
+        max-width: 600px;
+        margin: 0 auto;
+        padding: 20px;
+        background-color: #fff;
+        border-radius: 8px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+      }
+      h1 {
+        color: #333;
+      }
+      p {
+        color: #666;
+        margin-bottom: 20px;
+      }
+     
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <h1>¡Hola administrador!</h1>
+      <p>Hemos recibido un nuevo contacto para agregar a la WhiteList.</p>
+      <h2>${email}</h2>
+      
+    </div>
+  </body>`,
     });
     console.log(result);
     res.json({ status: "success", message: "correo enviado" });
