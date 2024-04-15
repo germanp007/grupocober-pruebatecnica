c4333dbcbba635b20179a840950f2231dd0fbafe
germanp007 <pintogerman281@gmail.com>
2024-04-14 21:39:15 -0300

    creando persistencia dark/light mode

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -1,25 +1,47 @@
 import sun from "../../public/images/sun.png";
 import moon from "../../public/images/moon.png";
-//import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function DarModeToggle() {
-  //const [checked, setChecked] = useState(false);
-  //const [theme, setTheme] = useState("light");
+  const [darkMode, setDarkMode] = useState(false);
 
-  const setDarkMode = () => {
+  const setDarkTheme = () => {
     document.querySelector("body").setAttribute("data-theme", "dark");
+    setDarkMode(true);
   };
-  const setLightMode = () => {
+  const setLightTheme = () => {
     document.querySelector("body").removeAttribute("data-theme", "dark");
+    setDarkMode(false);
   };
 
-  const setToggle = (e) => {
-    if (!e.target.checked) {
-      setDarkMode();
+  // Función para cambiar el modo al hacer clic en el interruptor
+  const setToggle = () => {
+    if (!darkMode) {
+      setDarkTheme();
+      localStorage.setItem("theme", "dark");
     } else {
-      setLightMode();
+      setLightTheme();
+      localStorage.setItem("theme", "light");
     }
   };
+
+  // Efecto para cargar el tema almacenado localmente al iniciar
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme === "dark") {
+      setDarkTheme();
+    } else {
+      setLightTheme();
+    }
+  }, []);
+
+  // const setToggle = (e) => {
+  //   if (!e.target.checked) {
+  //     setDarkMode();
+  //   } else {
+  //     setLightMode();
+  //   }
+  // };
 
   // useEffect(() => {
   //   const savedTheme = localStorage.getItem("theme");
@@ -42,6 +64,8 @@ export default function DarModeToggle() {
         type="checkbox"
         className="checkbox"
         id="checkbox"
+        // value={darkMode}
+        checked={!darkMode}
         onChange={setToggle}
       />
       <label htmlFor="checkbox" className="checkbox-label">
